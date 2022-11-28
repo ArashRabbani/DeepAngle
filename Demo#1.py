5412b86e69890f05701c00697f90dcad432467a9
ArashRabbani <rabarash@gmail.com>
2022-11-28 16:49:46 +0000

    Code Updated.

--- Demo#1.py
+++ Demo#1.py
@@ -1,4 +1,5 @@
 import deepangle as da
+# Name of the hdf5 file containing the volumetric image (h5 filed name: /Map)
 Data='Data/Data1.h5';
 Angles,Coordinates=da.getangle(Data,Mode='contact',Fast=1)
 da.hist(Angles)
