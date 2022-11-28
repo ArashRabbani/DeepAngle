a58a45a58b72e334395fe35d4431017ed5343314
ArashRabbani <rabarash@gmail.com>
2022-11-28 17:03:32 +0000

    Code Updated.

--- Demo#1.py
+++ Demo#1.py
@@ -2,6 +2,8 @@ import deepangle as da
 # Name of the hdf5 file containing the volumetric image (h5 filed name: /Map)
 Data='Data/Data1.h5';
 # Extracting contact angles and corresponding points 
-Angles,Coordinates=da.getangle(Data,Mode='contact',Fast=1)
+Angles,Coordinates=da.getangle(Data)
+# Plotting the angle distribution
 da.hist(Angles)
+# Exporting the results as a text file
 da.saveresults(Angles,Coordinates,'Output1')
